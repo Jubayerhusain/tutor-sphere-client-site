72eb82a43931ed7d8381deab29cbf1e1e9566ee3
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-22 17:54:00 +0600

    Navbar drwopdown done with responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
               : "text-lg hover:bg-blue-100 text-gray-500 font-semibold hover:text-blue-500 hover:underline"
           }
         >
-          Add Tutorial 
+          Add Tutorial
         </NavLink>
       </li>
       <li>
@@ -67,7 +67,7 @@ function Navbar() {
     </>
   );
   return (
-    <div className="sticky top-0 z-50 shadow-2xl mb-2">
+    <div className="sticky top-0 z-50 shadow-2xl">
       <div className="navbar bg-base-100  xl:px-14 lg:px-7 shadow-2xl xl:py-4 py-2">
         <div className="navbar-start">
           <div className="dropdown">
@@ -99,8 +99,44 @@ function Navbar() {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{linkCenter}</ul>
         </div>
-        <div className="navbar-end">
-          <a className="text-xl font-semibold underline transition">Login</a>
+        <div className="navbar-end items-center space-x-1">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar hover:bg-blue-600  relative group"
+            >
+              <div className="w-14 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+                <div className="absolute left-1/2 -translate-x-1/2 bottom-[10px] px-3 py-1 text-sm font-medium text-white/80 rounded opacity-0 group-hover:opacity-100 transition">
+                  User Name
+                </div>
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+          <a className="text-xl font-semibold underline transition text-blue-500 border-2 rounded-md py-2 px-4 border-blue-600 hover:bg-blue-100">
+            Login
+          </a>
         </div>
       </div>
     </div>
