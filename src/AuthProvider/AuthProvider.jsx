bd0f62634464e1f60d471d0764833d2b4a81647b
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-22 22:45:28 +0600

    loading spinner addedd done

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -61,7 +61,7 @@ function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      console.log('hey juabyer your current user', currentUser);
+      console.log("hey juabyer your current user", currentUser);
       setLoading(false); // Stop loading once auth state is determined
     });
     return () => unsubscribe();
@@ -78,8 +78,11 @@ function AuthProvider({ children }) {
   return (
     <AuthContext.Provider value={authInfo}>
       {loading ? (
-        <div class="flex items-center justify-center h-screen ">
-          <div class="spinner animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
+        <div class="flex items-center justify-center h-screen bg-gray-100">
+          <div class="relative flex items-center justify-center">
+            <div class="animate-spin rounded-full h-20 w-20 border-t-4 border-blue-500 border-dotted"></div>
+            <div class="absolute inset-0 h-12 w-12 rounded-full border-4 border-gradient-to-r from-green-400 to-blue-500"></div>
+          </div>
         </div>
       ) : (
         children
