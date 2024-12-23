38a3988bfeffaf718cac907503c4cb3f8240c177
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-23 10:04:48 +0600

    languegesCategory getting done from server site

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Bannar from './../../Components/Bannar/Bannar';
+import React, { useContext } from "react";
+import Bannar from "./../../Components/Bannar/Bannar";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 function Home() {
+  const { loading } = useContext(AuthContext);
+  const languegesCategory = useLoaderData();
+//   const { logo, title, totalTutors } = languegesCategory;
+  if (loading || !languegesCategory || !Array.isArray(languegesCategory)) {
     return (
-        <div className='min-h-screen'>
-            <Bannar></Bannar>
+      <div class="flex items-center justify-center h-screen bg-gray-100">
+        <div class="relative flex items-center justify-center">
+          <div class="animate-spin rounded-full h-20 w-20 border-t-4 border-blue-500 border-dotted"></div>
+          <div class="absolute inset-0 h-12 w-12 rounded-full border-4 border-gradient-to-r from-green-400 to-blue-500"></div>
         </div>
-    )
+      </div>
+    );
+  }
+  console.log(languegesCategory);
+  return (
+    <div className="min-h-screen">
+      <Bannar></Bannar>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -18,6 +18,8 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+        loader: () =>
+          fetch(`https://tutor-sphere-server-side.vercel.app/langueges`),
       },
       {
         path: "/findTutors",
