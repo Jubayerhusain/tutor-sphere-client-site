2c93ce1788e815bf840ea545002d46ef5641ba8e
Jubayer husain <jubayerhusai360@gmail.com>
2025-01-11 14:54:25 +0600

    Simple Steps to Academic Success done

--- src/Components/HowItWorks/HowItWorks.jsx
+++ src/Components/HowItWorks/HowItWorks.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HowItWorks = () => {
   return (
-    <div>
+    <div className="max-w-screen-lg mx-auto px-4">
       <div className="flex flex-col justify-center space-y-2 my-14">
         <p className="text-center font-semibold text-xl">How it Work</p>
         <h1 className="text-center font-bold text-4xl uppercase">
@@ -11,9 +11,9 @@ const HowItWorks = () => {
       </div>
       <ul className="timeline pb-14 timeline-vertical">
         <li>
-          <div className="timeline-start timeline-box">
-            <div className="flex items-center space-x-3 m-5">
-              <div className="ml-4">
+          <div className="timeline-start timeline-box p-5 border rounded-lg">
+            <div className="flex flex-wrap md:flex-nowrap items-center space-y-3 md:space-y-0 md:space-x-3">
+              <div className="w-full md:w-3/4">
                 <h1 className="text-end text-xl font-semibold text-yellow-500">
                   Book a Free Consultation
                 </h1>
@@ -23,45 +23,41 @@ const HowItWorks = () => {
                 </p>
               </div>
               <img
-                width="64"
-                height="64"
+                className="w-16 h-16"
                 src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-consultation-psychology-flaticons-flat-flat-icons.png"
-                alt="external-consultation-psychology-flaticons-flat-flat-icons"
+                alt="Consultation"
               />
             </div>
           </div>
-          <hr />
+          <hr className="my-4" />
         </li>
         <li>
-          <hr />
-          <div className="timeline-end timeline-box">
-            <div className="flex items-center space-x-3 m-5">
+          <div className="timeline-end timeline-box p-5 border rounded-lg">
+            <div className="flex flex-wrap md:flex-nowrap items-center space-y-3 md:space-y-0 md:space-x-3">
               <img
-                width="100"
-                height="100"
+                className="w-20 h-20"
                 src="https://img.icons8.com/plasticine/100/classroom.png"
-                alt="classroom"
+                alt="Classroom"
               />
-              <div className="ml-4">
+              <div className="w-full md:w-3/4">
                 <h1 className="text-start text-xl font-semibold text-yellow-500">
                   Personalized Lesson Plan
                 </h1>
                 <p>
                   After the consultation, we'll create a tailored lesson plan
-                  for you, outlining topics, timeline and resources.
+                  for you, outlining topics, timeline, and resources.
                 </p>
               </div>
             </div>
           </div>
-          <hr />
+          <hr className="my-4" />
         </li>
         <li>
-          <hr />
-          <div className="timeline-start timeline-box">
-            <div className="flex items-center space-x-3 m-5">
-              <div className="ml-4">
+          <div className="timeline-start timeline-box p-5 border rounded-lg">
+            <div className="flex flex-wrap md:flex-nowrap items-center space-y-3 md:space-y-0 md:space-x-3">
+              <div className="w-full md:w-3/4">
                 <h1 className="text-end text-xl font-semibold text-yellow-500">
-                  Match with a specialist
+                  Match with a Specialist
                 </h1>
                 <p>
                   We'll connect you with a subject expert to guide your learning
@@ -69,43 +65,39 @@ const HowItWorks = () => {
                 </p>
               </div>
               <img
-                width="48"
-                height="48"
+                className="w-12 h-12"
                 src="https://img.icons8.com/color/48/pull-a-box-skin-type-5.png"
-                alt="pull-a-box-skin-type-5"
+                alt="Specialist"
               />
             </div>
           </div>
-          <hr />
+          <hr className="my-4" />
         </li>
         <li>
-          <hr />
-          <div className="timeline-end timeline-box">
-            <div className="flex items-center space-x-3 m-5">
+          <div className="timeline-end timeline-box p-5 border rounded-lg">
+            <div className="flex flex-wrap md:flex-nowrap items-center space-y-3 md:space-y-0 md:space-x-3">
               <img
-                width="48"
-                height="48"
+                className="w-12 h-12"
                 src="https://img.icons8.com/color/48/book-and-pencil.png"
-                alt="book-and-pencil"
+                alt="Start Learning"
               />
-              <div className="ml-4">
+              <div className="w-full md:w-3/4">
                 <h1 className="text-start text-xl font-semibold text-yellow-500">
                   Start Learning
                 </h1>
                 <p>
                   Begin interactive and personalized tutoring sessions from the
-                  comfort of your home
+                  comfort of your home.
                 </p>
               </div>
             </div>
           </div>
-          <hr />
+          <hr className="my-4" />
         </li>
         <li>
-          <hr />
-          <div className="timeline-start timeline-box">
-            <div className="flex items-center space-x-3 m-5">
-              <div className="ml-4">
+          <div className="timeline-start timeline-box p-5 border rounded-lg">
+            <div className="flex flex-wrap md:flex-nowrap items-center space-y-3 md:space-y-0 md:space-x-3">
+              <div className="w-full md:w-3/4">
                 <h1 className="text-end text-xl font-semibold text-yellow-500">
                   Ongoing Support
                 </h1>
@@ -115,25 +107,23 @@ const HowItWorks = () => {
                 </p>
               </div>
               <img
-                width="64"
-                height="64"
+                className="w-16 h-16"
                 src="https://img.icons8.com/arcade/64/helping.png"
-                alt="helping"
+                alt="Helping"
               />
             </div>
           </div>
+          <hr className="my-4" />
         </li>
         <li>
-          <hr />
-          <div className="timeline-end timeline-box">
-            <div className="flex items-center space-x-3 m-5">
+          <div className="timeline-end timeline-box p-5 border rounded-lg">
+            <div className="flex flex-wrap md:flex-nowrap items-center space-y-3 md:space-y-0 md:space-x-3">
               <img
-                width="100"
-                height="100"
+                className="w-20 h-20"
                 src="https://img.icons8.com/plasticine/100/favorite-window.png"
-                alt="favorite-window"
+                alt="Review & Improve"
               />
-              <div className="ml-4">
+              <div className="w-full md:w-3/4">
                 <h1 className="text-start text-xl font-semibold text-yellow-500">
                   Review & Improve
                 </h1>
@@ -144,7 +134,6 @@ const HowItWorks = () => {
               </div>
             </div>
           </div>
-          <hr />
         </li>
       </ul>
     </div>
